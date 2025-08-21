e12bab69993fda38576b7c3ebe4d2f97be963d8d
Dede Rahmat <dederamat102@gmail.com>
2025-08-21 15:52:57 +0700

    first commit

---
+++ src/index.mjs
@@ -0,0 +1,30 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import mongoose from 'mongoose';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+
+mongoose.connect(`${process.env.DBHOST}://${process.env.DBUSER}:${process.env.DBPASSWORD}@anynote.kadvjky.mongodb.net/?retryWrites=true&w=majority&appName=anynote`)
+  .then(() => console.log('Connected to database!.'))
+  .catch((err) => console.log(`Error: ${err}`));
+
+app.use(express.json());
+app.use(cookieParser(process.env.COOKIE_KEY));
+
+const HOST = process.env.HOST || 'localhost';
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.status(201).send({ msg: 'Hello!'});
+});
+
+app.get('/users/:name', (req, res) => {
+  return res.status(201).send({ msg: 'Hello!'});
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`Server running at http://${HOST}:${PORT}`);
+});
