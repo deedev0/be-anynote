e16f869a706a1cb6f2e9c7c4d39433845f789f73
Dede Rahmat <dederamat102@gmail.com>
2025-09-09 11:19:30 +0700

    fixing session3

--- src/index.mjs
+++ src/index.mjs
@@ -37,7 +37,7 @@ app.use(session({
     httpOnly: true,
     maxAge: 1000 * 60 * 60 * 48,
     sameSite: 'none',
-    secure: true,
+    secure: false,
   },
   store: MongoStore.create({
     client: mongoose.connection.getClient(),
