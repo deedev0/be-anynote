8fedc49df21d44b08c7f382f225587266434a41b
Dede Rahmat <dederamat102@gmail.com>
2025-09-09 10:44:02 +0700

    fixing session2

--- src/middlewares/auth/checkAuthentication.mjs
+++ src/middlewares/auth/checkAuthentication.mjs
@@ -1,4 +1,6 @@
 export const checkAuthentication = (req, res, next) => {
+  console.log(req.session);
+  console.log(req.session.user);
   if (!req.session.user) return res.status(401).send({ message: 'Not Authenticated'}); 
 
   next();
